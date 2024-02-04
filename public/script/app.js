9d256cd4ed0c449afaef56066b6bc18c3f6dc350
omid-tavakoli <omid37268082@gamil.com>
2024-02-05 01:10:03 +0330

    edit in toggle

--- public/script/app.js
+++ public/script/app.js
@@ -1,10 +1,12 @@
 const contact = document.querySelector('.contact')
-
+const backDrop = document.querySelector('.back-drop')
 function openContact(){
     contact.classList.add('flex')
     contact.classList.remove('hidden')
+    backDrop.classList.remove('hidden')
 }
 function closeContact(){
     contact.classList.add('hidden')
     contact.classList.remove('flex')
+    backDrop.classList.add('hidden')
 }
